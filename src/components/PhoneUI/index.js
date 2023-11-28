0fd147015af3e08abdd5b3c1c1c0fb1a0cc6b2ac
sonu7524 <sonudaryani135@gmail.com>
2023-11-28 21:23:19 +0530

    update

--- src/components/PhoneUI/index.js
+++ src/components/PhoneUI/index.js
@@ -1,6 +1,7 @@
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
+import "./styles.css";
 
 export default function PhoneUI() {
     const query = useStaticQuery(graphql`
@@ -22,15 +23,17 @@ export default function PhoneUI() {
   }
 `);
     const data = query.allMarkdownRemark.edges;
-    return <div className="phone-ui">
-        {data.map(({node})=> (
-            node.frontmatter.display_picture !== null ? (
-                <GatsbyImage
-                    image={getImage(node.frontmatter.display_picture)}
-                    alt="display_picture"
-                />
-            ) : null
-        ))}
-    </div>;
+    return (
+        <div className="phone-ui">
+            {data.map(({node})=> (
+                node.frontmatter.display_picture !== null ? (
+                    <GatsbyImage
+                        image={getImage(node.frontmatter.display_picture)}
+                        alt="display_picture"
+                    />
+                ) : null
+            ))}
+        </div>
+    )
 }
 
